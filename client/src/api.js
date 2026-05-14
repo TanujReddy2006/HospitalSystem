f880a8efd6d9f199c787bc10f3a34452296c0027
TanujReddy2006 <vakadatanujreddy2006@gmail.com>
2026-05-14 15:23:51 +0530

    old

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://hospitalsystem-qxtk.onrender.com/api"
 });
-
+{/*hello*/}
 api.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
